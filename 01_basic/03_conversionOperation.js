b72c98a2b05715554a9cb40efe67d13f3f869c7d
Himanshu1073204 <himanshu.kr104@gmail.com>
2025-07-18 06:50:06 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -22,6 +22,52 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "hello" => true
 
 
-let x = 33
-let stringx = String(x)
-console.log(typeof stringx);
+let z = 33
+let stringz = String(z)
+// console.log(stringx);
+// console.log(typeof stringx);
+  
+
+// ************************************* Operations ***********
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " moto"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let first = 3;
+const second = ++x;
+// first is 4; second is 4
