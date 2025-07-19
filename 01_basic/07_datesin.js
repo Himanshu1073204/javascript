71c2245bda91a474253755147499913edaebec55
Himanshu1073204 <himanshu.kr104@gmail.com>
2025-07-19 09:32:10 +0000

    Date

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+let createdDate = new Date(2023, 0, 23)
+// console.log(createdDate);
+
+let mycreatedDate = new Date("01-04-2025")
+// console.log(mycreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
